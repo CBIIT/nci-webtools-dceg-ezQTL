b38e0b3104c52d9577f29e06b1f7c83369c0d1c3
Kevin Jiang <21976531+kjiang95@users.noreply.github.com>
2018-08-23 12:01:48 -0400

    change path to relative, use hash for routes

--- src/app/router/app-routing.module.ts
+++ src/app/router/app-routing.module.ts
@@ -21,7 +21,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [ RouterModule.forRoot(routes) ],
+  imports: [ RouterModule.forRoot(routes, {useHash: true}) ],
   exports: [ RouterModule ]
 })
 
