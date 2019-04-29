8ed79f70736a4c5ad6d7ebee8b6f4a896032cced
kvnjng <kvvnjng@gmail.com>
2019-04-29 11:46:24 -0400

    add rsnum search to locuszoom plot and populate variant details table

--- src/app/components/eqtl-results-table/eqtl-results-table.component.ts
+++ src/app/components/eqtl-results-table/eqtl-results-table.component.ts
@@ -39,9 +39,11 @@ export interface Variant {
 export class EqtlResultsTableComponent implements OnInit {
 
   locuszoomData: Object;
+  locuszoomDataQTopAnnot: Object;
   VARIANT_DATA: Variant[];
-  
-  displayedColumns: string[] = ['gene_id', 'gene_symbol', 'variant_id', 'rsnum', 'chr', 'pos', 'ref', 'alt', 'tss_distance', 'pval_nominal', 'slope', 'slope_se', 'R2'];
+  selectedPop: string[];
+  newSelectedPop: string;
+  displayedColumns: string[] = ['gene_id', 'gene_symbol', 'variant_id', 'rsnum', 'chr', 'pos', 'ref', 'alt', 'tss_distance', 'pval_nominal', 'slope', 'slope_se', 'R2', 'LDpop', 'GWAS', 'genomAD'];
   dataSource = new MatTableDataSource<Variant>(this.VARIANT_DATA);
 
   @ViewChild(MatPaginator) paginator: MatPaginator;
@@ -53,7 +55,11 @@ export class EqtlResultsTableComponent implements OnInit {
     this.data.currentMainData.subscribe(mainData => {
       if (mainData) {
         this.locuszoomData = mainData["locuszoom"]["data"][0]; // locuszoom data
+        this.newSelectedPop = mainData["info"]["inputs"]["select_pop"][0]; // inputted populations
+        this.locuszoomDataQTopAnnot = mainData["locuszoom"]["top"][0][0]; // locuszoom Top Gene data
       }
+      this.selectedPop = this.newSelectedPop.split('+');; // recalculated new population selection
+      this.expandPopulationGroup();
       if (this.locuszoomData) {
         this.VARIANT_DATA = this.populateVariantDataList(this.locuszoomData);
       }
@@ -80,14 +86,98 @@ export class EqtlResultsTableComponent implements OnInit {
       variant['slope'] = geneData[i]['slope'];
       variant['slope_se'] = geneData[i]['slope_se'];
       variant['R2'] = geneData[i]['R2'];
+      variant['LDpop'] = "Link";
+      variant['GWAS'] = "Link";
+      variant['genomAD'] = "Link";
       data.push(variant);
     }
-    console.log(data);
+    // console.log(data);
     return data;
   }
   
   applyFilter(filterValue: string) {
     this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
+
+  linkLDpop(rsnum) {
+    var QTopAnnotRef = this.locuszoomDataQTopAnnot["rsnum"];
+    var selectedPopString = this.selectedPop.join('%2B');
+    var url = "https://ldlink.nci.nih.gov/?tab=ldpop&var1=" + rsnum + "&var2=" + QTopAnnotRef + "&pop=" + selectedPopString + "&r2_d=r2"
+    var win = window.open(url, '_blank');
+    win.focus();
+  } 
+
+  linkGWAS(rsnum) {
+    var url = "https://www.ebi.ac.uk/gwas/search?query=" + rsnum
+    var win = window.open(url, '_blank');
+    win.focus();
+  }
+
+  linkGenomADBrowser(chr, pos, ref, alt) {
+    var url = "http://gnomad.broadinstitute.org/variant/" + chr + "-" + pos + "-" + ref + "-" + alt
+    var win = window.open(url, '_blank');
+    win.focus();
+  }
+
+  unique(value, index, self) {
+    return self.indexOf(value) === index;
+  }
+
+  containsAll(subarr, arr) {
+    for (var i = 0, len = subarr.length; i < len; i++) {
+      if (!arr.includes(subarr[i])) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  remove(element, src) {
+    var newArray = JSON.parse(JSON.stringify(src));
+    // console.log(newArray);
+    for (var i = 0; i < newArray.length; i++) {
+      var idx = -1;
+      if (newArray[i] == element) {
+        idx = i;
+      }
+      if (idx != -1) {
+        newArray.splice(idx, 1);
+      }
+    }
+    return newArray;
+  }
+
+  expandPopulationGroup() {
+    var african = ["YRI", "LWK", "GWD", "MSL", "ESN", "ASW", "ACB"];
+    var mixedAmerican = ["MXL", "PUR", "CLM", "PEL"];
+    var eastAsian = ["CHB", "JPT", "CHS", "CDX", "KHV"];
+    var european = ["CEU", "TSI", "FIN", "GBR", "IBS"];
+    var southAsian = ["GIH", "PJL", "BEB", "STU", "ITU"];
+    // AFR
+    if (this.selectedPop.includes("AFR")) {
+      this.selectedPop = this.remove("AFR", this.selectedPop);
+      this.selectedPop = (this.selectedPop.concat(african)).filter(this.unique);
+    }
+    // AMR
+    if (this.selectedPop.includes("AMR")) {
+      this.selectedPop = this.remove("AMR", this.selectedPop);
+      this.selectedPop = (this.selectedPop.concat(mixedAmerican)).filter(this.unique);
+    }
+    // EAS
+    if (this.selectedPop.includes("EAS")) {
+      this.selectedPop = this.remove("EAS", this.selectedPop);
+      this.selectedPop = (this.selectedPop.concat(eastAsian)).filter(this.unique);
+    }
+    // EUR
+    if (this.selectedPop.includes("EUR")) {
+      this.selectedPop = this.remove("EUR", this.selectedPop);
+      this.selectedPop = (this.selectedPop.concat(european)).filter(this.unique);
+    }
+    // SAS
+    if (this.selectedPop.includes("SAS")) {
+      this.selectedPop = this.remove("SAS", this.selectedPop);
+      this.selectedPop = (this.selectedPop.concat(southAsian)).filter(this.unique);
+    }
   }
 
 }
