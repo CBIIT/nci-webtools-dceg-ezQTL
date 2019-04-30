3cf121b5f2bd8f1d981fb66993589238cf3d3ebc
kvnjng <kvvnjng@gmail.com>
2019-04-30 11:13:36 -0400

    add warning message if searched rsnum not found in Locuszoom Plot

--- src/app/components/eqtl-results-table/eqtl-results-table.component.ts
+++ src/app/components/eqtl-results-table/eqtl-results-table.component.ts
@@ -19,19 +19,6 @@ export interface Variant {
   R2: string;
 }
 
-// const VARIANT_DATA: Variant[] = [
-//   {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-//   {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
-//   {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-//   {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-//   {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
-//   {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
-//   {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
-//   {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
-//   {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
-//   {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
-// ];
-
 @Component({
   selector: 'app-eqtl-results-table',
   templateUrl: './eqtl-results-table.component.html',
@@ -148,7 +135,6 @@ export class EqtlResultsTableComponent implements OnInit {
 
   downloadTable() {
     var url = environment.endpoint + "tmp/" + this.requestID + ".variant_details.txt";
-    console.log(url);
     var win = window.open(url, '_blank');
     win.focus();
   } 
