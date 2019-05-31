1689b8939f5e6e01daaf69c214cf45e5ab299783
kvnjng <kvvnjng@gmail.com>
2019-05-31 16:24:18 -0400

    limit QTLs association points cis-QTL Distance window size

--- r-calculations/QTLs/qtls.r
+++ r-calculations/QTLs/qtls.r
@@ -161,6 +161,9 @@ locus_alignment <- function(workDir, select_gwas_sample, qdata, qdata_tmp, kgpan
   # chromosome <- unique(qdata$chr)
   # minpos <- min(qdata$pos)
   # maxpos <- max(qdata$pos)
+  
+  ## subset gene variant with cis-QTL Distance window
+  qdata <- subset(qdata, pos > minpos & pos < maxpos)
 
   kgvcfpath <- paste0(workDir, '/data/1000G/ALL.chr', chromosome, '.phase3_shapeit2_mvncall_integrated_v5a.20130502.genotypes.vcf.gz')
   
