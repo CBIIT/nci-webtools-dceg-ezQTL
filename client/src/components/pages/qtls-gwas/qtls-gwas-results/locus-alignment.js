51ce55ef11d19ed0f21e4c60492328cf6e09bb7f
kvnjng <kvvnjng@gmail.com>
2021-03-10 17:01:22 -0700

    move action functions to separate actions file

--- client/src/components/pages/qtls-gwas/qtls-gwas-results/locus-alignment.js
+++ client/src/components/pages/qtls-gwas/qtls-gwas-results/locus-alignment.js
@@ -1,9 +1,39 @@
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { LoadingOverlay } from '../../../controls/loading-overlay/loading-overlay';
 
 export function LocusAlignment() {
+    const {
+        loadSampleQTLs,
+        loadSampleGWAS,
+        associationFile,
+        quantificationFile,
+        genotypeFile,
+        ldFile,
+        gwasFile,
+        distance,
+        refSNP,
+        population,
+        refGene,
+        refSNPPost,
+        submitted,
+        isLoading
+    } = useSelector(({ezQTL}) => ezQTL.qtlsGWAS);
+
     return (
-        <div className="p-2">
-            Locus Alignment
+        <div className="p-2" style={{minHeight: '500px'}}>
+            {
+                !submitted &&
+                <LoadingOverlay active={true} content={'Select data in the left panel and click Calculate to see results here.'} />
+            }
+            {
+                submitted && 
+                (
+                    <>
+                        Locus Alignment RESULTS
+                    </>
+                )
+            }
         </div>
     )
 }
