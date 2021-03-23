864916b9536e4598685be32062c4cd27d8bc5482
Phyllip Cho <phyllip3@users.noreply.github.com>
2021-03-23 13:58:16 -0400

    prettier format

--- server/services/calculate.js
+++ server/services/calculate.js
@@ -3,65 +3,67 @@ const path = require('path');
 const logger = require('../services/logger');
 
 async function qtlsCalculateMain(params, res, next) {
-    const {
-        request,
-        select_qtls_samples,
-        select_gwas_sample,
-        associationFile,
-        quantificationFile,
-        genotypeFile,
-        gwasFile,
-        LDFile,
-        select_pop,
-        select_gene,
-        select_dist,
-        select_ref,
-        recalculateAttempt,
-        recalculatePop,
-        recalculateGene,
-        recalculateDist,
-        recalculateRef,
-        workingDirectory
-    } = params;
+  const {
+    request,
+    select_qtls_samples,
+    select_gwas_sample,
+    associationFile,
+    quantificationFile,
+    genotypeFile,
+    gwasFile,
+    LDFile,
+    select_pop,
+    select_gene,
+    select_dist,
+    select_ref,
+    recalculateAttempt,
+    recalculatePop,
+    recalculateGene,
+    recalculateDist,
+    recalculateRef,
+    workingDirectory,
+  } = params;
 
-    logger.info(`[${request}] Execute /qtls-calculate-main`);
-    logger.debug(`[${request}] Parameters ${JSON.stringify(params, undefined, 4)}`);
+  logger.info(`[${request}] Execute /qtls-calculate-main`);
+  logger.debug(
+    `[${request}] Parameters ${JSON.stringify(params, undefined, 4)}`
+  );
 
-    const rfile = path.resolve(__dirname, 'query_scripts', 'QTLs', 'qtls.r');
-    try {
-        const wrapper = await r(
-            path.resolve(__dirname, 'query_scripts', 'wrapper.R'),
-            "qtlsCalculateMain",
-            [
-                rfile,
-                workingDirectory.toString(),
-                select_qtls_samples.toString(),
-                select_gwas_sample.toString(),
-                associationFile.toString(),
-                quantificationFile.toString(),
-                genotypeFile.toString(),
-                gwasFile.toString(),
-                LDFile.toString(),
-                request.toString(),
-                select_pop.toString(),
-                select_gene.toString(),
-                select_dist.toString(),
-                select_ref.toString(),
-                recalculateAttempt.toString(),
-                recalculatePop.toString(),
-                recalculateGene.toString(),
-                recalculateDist.toString(),
-                recalculateRef.toString()
-            ]
-        );
-        logger.info(`[${request}] Finished /qtls-calculate-main`);
-        res.json(JSON.parse(wrapper));
-    } catch (err) {
-        logger.error(`[${request}] Error /qtls-calculate-main ${err}`);
-        res.status(500).json(err);
-    }
+  const rfile = path.resolve(__dirname, 'query_scripts', 'QTLs', 'qtls.r');
+  try {
+    const wrapper = await r(
+      path.resolve(__dirname, 'query_scripts', 'wrapper.R'),
+      'qtlsCalculateMain',
+      [
+        rfile,
+        workingDirectory.toString(),
+        select_qtls_samples.toString(),
+        select_gwas_sample.toString(),
+        associationFile.toString(),
+        quantificationFile.toString(),
+        genotypeFile.toString(),
+        gwasFile.toString(),
+        LDFile.toString(),
+        request.toString(),
+        select_pop.toString(),
+        select_gene.toString(),
+        select_dist.toString(),
+        select_ref.toString(),
+        recalculateAttempt.toString(),
+        recalculatePop.toString(),
+        recalculateGene.toString(),
+        recalculateDist.toString(),
+        recalculateRef.toString(),
+      ]
+    );
+    logger.info(`[${request}] Finished /qtls-calculate-main`);
+    res.json(JSON.parse(wrapper));
+  } catch (err) {
+    logger.error(`[${request}] Error /qtls-calculate-main ${err}`);
+    res.status(500).json(err);
+  }
 }
 
 module.exports = {
-    qtlsCalculateMain
-}
+  qtlsCalculateMain,
+};
