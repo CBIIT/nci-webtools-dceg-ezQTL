3af2e5001959ff40b343117fd008195e3ae0c9f1
bchen49 <benchen4998@gmail.com>
2021-05-12 14:48:32 -0400

    retrieve public data for QC function

--- server/services/query_scripts/wrapper.R
+++ server/services/query_scripts/wrapper.R
@@ -44,18 +44,41 @@ qtlsCalculateLocusColocalizationHyprcoloc <- function(rfile, workingDirectory, s
   locus_colocalization_hyprcoloc(workingDirectory, select_gwas_sample, select_qtls_samples, select_dist, select_ref, gwasFile, associationFile, ldfile, request, qtlKey, select_chromosome, select_position, bucket)
 }
 
-qtlsCalculateQC <- function(rfile, select_gwas_sample, select_qtls_samples, gwasFile, associationFile, ldFile, leadsnp, distance, zscore_gene, request, plotPath, inputPath, logPath, workDir, bucket) {
+qtlsCalculateQC <- function(rfile, select_gwas_sample, select_qtls_samples, gwasFile, associationFile, ldFile, qtlKey, gwasKey, ldKey, leadsnp, distance, select_chromosome, select_position, ldProject, request, plotPath, inputPath, logPath, workDir, bucket) {
   source(rfile)
   library(data.table)
+  source(paste0(workDir, '/', 'server/', 'services/', 'query_scripts/', 'QTLs/', 'qtls.r'))
+
+  if (identical(distance, 'false')) {
+    ## set default cisDistance to 100 Kb (100,000 bp) if none supplied
+    cedistance <- 100 * 1000
+  } else {
+    cedistance <- strtoi(distance, base = 0L) * 1000
+  }
+
+  select_position = strtoi(select_position, base = 0L)
+  minpos = ifelse(select_position - cedistance < 0, 0, select_position - cedistance)
+  maxpos = select_position + cedistance
 
   if (identical(select_gwas_sample, 'true')) {
     gwasFile <- getS3File('ezQTL/MX2.examples/MX2.GWAS.rs.txt', bucket)
   } else {
 
-    if(identical(gwasFile, 'false'))
-      gwasFile <- NULL
-    else 
-      gwasFile <- paste0(workDir, '/tmp/', request, '/', gwasFile)
+    if(identical(gwasKey, 'false')){
+
+      if(identical(gwasFile, 'false'))
+        gwasFile <- NULL
+      else 
+        gwasFile <- paste0(workDir, '/tmp/', request, '/', gwasFile)
+    }
+    else{
+      loadAWS()
+      gwasPathS3 = paste0('s3://', bucket, '/ezQTL/', gwasKey)
+      gwasFile = paste0(request, ".gwas_temp.txt")
+      cmd = paste0("cd data/", dirname(gwasKey), "; tabix ", gwasPathS3, " ", select_chromosome, ":", minpos, '-', maxpos, " -Dh >", workDir, "/tmp/", request, '/', gwasFile)
+      system(cmd)
+      gwasFile <- paste0('tmp/', request, '/', request, '.', 'gwas_temp', '.txt')
+    }
   }
   if (identical(select_qtls_samples, 'true')) {
     associationFile <- getS3File('ezQTL/MX2.examples/MX2.eQTL.txt', bucket)
@@ -66,18 +89,37 @@ qtlsCalculateQC <- function(rfile, select_gwas_sample, select_qtls_samples, gwas
     save_object(publicLDFile, bucket, file = ldFile)
   } else {
 
-    if(identical(associationFile, 'false'))
-      associationFile = NULL
-    else
-      associationFile <- paste0(workDir, '/tmp/', request, '/', associationFile)
+    if(identical(gwasKey, 'false')){
 
-    if(identical(ldFile, 'false'))
-      ldFile = NULL
-    else
-      ldFile <- paste0(workDir, '/tmp/', request, '/', ldFile)
+      if(identical(associationFile, 'false'))
+        associationFile = NULL
+      else
+        associationFile <- paste0(workDir, '/tmp/', request, '/', associationFile)
+    }
+    else{
+      loadAWS()
+      qtlPathS3 = paste0('s3://', bucket, '/ezQTL/', qtlKey)
+      assocFile = paste0(request, ".qtl_temp.txt")
+      cmd = paste0("cd data/", dirname(qtlKey), "; tabix ", qtlPathS3, " ", select_chromosome, ":", minpos, '-', maxpos, " -Dh >", workDir, "/tmp/", request, '/', assocFile)
+      system(cmd)
+      associationFile <- paste0('tmp/', request, '/', request, '.', 'qtl_temp', '.txt')
+    }
+    if(identical(ldKey, 'false')){
+      if(identical(ldFile, 'false'))
+        ldFile = NULL
+      else
+        ldFile <- paste0(workDir, '/tmp/', request, '/', ldFile)
+    }
+    else{
+      getPublicLD(bucket, ldKey, request, select_chromosome, minpos, maxpos, ldProject)
+      ldFile <- paste0('tmp/', request, '/', request, '.input.vcf.gz')
+    }
   }
 
-  coloc_QC(gwasFile, TRUE, associationFile, TRUE, ldFile, TRUE, leadsnp, NULL, distance, zscore_gene, plotPath, inputPath, logPath)
+  if(identical(leadsnp,'false'))
+    leadsnp <- NULL
+
+  coloc_QC(gwasFile, TRUE, associationFile, TRUE, ldFile, TRUE, leadsnp, NULL, cedistance, NULL, plotPath, inputPath, logPath)
 }
 
 qtlsColocVisualize <- function(rfile, hydata, ecdata, request) {
@@ -117,6 +159,10 @@ qtlsCalculateLD <- function(rfile, select_gwas_sample, select_qtls_samples, gwas
     else
       ldFile <- paste0(workDir, '/tmp/', request, '/ezQTL_input_ld.gz')
   }
+
+  print(gwasFile)
+  print(ldFile)
+  print(associationFile)
 
   if(!is.null(gwasFile))
     IntRegionalPlot(genome_build = 'GRCh37',association_file = gwasFile,LDfile = ldFile,gtf_tabix_folder = tabixPath,output_file = outputPath,leadsnp = leadsnp, threshold = 5,label_gene_name = TRUE)
