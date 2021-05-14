54b5e44e551b402d2d76a8a15a70204930e86b65
Phyllip Cho <phyllip3@users.noreply.github.com>
2021-05-14 16:20:38 -0400

    load sample data files instead of restoring sample results

--- client/src/components/pages/qtls-gwas/qtls-gwas-form.js
+++ client/src/components/pages/qtls-gwas/qtls-gwas-form.js
@@ -1134,11 +1134,11 @@ export function QTLsGWASForm() {
       </Row>
       <Row>
         <Col sm="6">
-          <Link to={`/qtls/sample`} className="font-14">
+          {/* <Link to={`/qtls/sample`} className="font-14">
             <span className="sr-only">Load Sample Data</span>
             Load Sample Data
-          </Link>
-          {/* <Button
+          </Link> */}
+          <Button
             className="p-0 font-14"
             variant="link"
             onClick={() => {
@@ -1167,7 +1167,7 @@ export function QTLsGWASForm() {
             disabled={submitted}
           >
             {!select_gwas_sample ? 'Load' : 'Unload'} Sample Data
-          </Button> */}
+          </Button>
         </Col>
 
         <Col sm="6" className="d-flex">
