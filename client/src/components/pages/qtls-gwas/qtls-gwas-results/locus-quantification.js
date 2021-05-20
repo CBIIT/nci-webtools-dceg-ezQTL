04f5f3c89fdb0704f61dd0f3229ab05d65807ddd
bchen49 <benchen4998@gmail.com>
2021-05-20 14:36:17 -0400

    remove quantification tab text

--- client/src/components/pages/qtls-gwas/qtls-gwas-results/locus-quantification.js
+++ client/src/components/pages/qtls-gwas/qtls-gwas-results/locus-quantification.js
@@ -10,11 +10,6 @@ export function LocusQuantifiation() {
 
   return (
     <div className="px-3 py-2" style={{ minHeight: '500px' }}>
-      <p>
-        Top 1 to 15 highest quantified traits (such as gene expression, methylation probe, and splicing junction)
-        will be shown in the following violin plots, which can be sorted and log transformed.
-      </p>
-
       <Zoom
         plotURL={`api/results/${request}/quantification_cor.svg`}
         className="border rounded p-3 mb-2"
