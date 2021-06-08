31ee8ce6d53866ddc7bfc74dd2c100660747f452
Phyllip Cho <phyllip3@users.noreply.github.com>
2021-06-08 17:01:03 -0400

    add render conditions to plots

--- client/src/components/pages/qtls-gwas/qtls-gwas-results/locus-quantification.js
+++ client/src/components/pages/qtls-gwas/qtls-gwas-results/locus-quantification.js
@@ -40,123 +40,139 @@ export function LocusQuantifiation() {
 
   return (
     <div className="px-3 py-2" style={{ minHeight: '500px' }}>
-      <LoadingOverlay active={isLoadingQuantification} />
-      <Zoom
-        plotURL={`api/results/${request}/quantification_cor.svg`}
-        className="border rounded p-3 mb-2"
-        maxHeight="800px"
-        descAbove={
+      {genotypeFile && quantificationFile && (
+        <div>
+          <LoadingOverlay active={isLoadingQuantification} />
+          <Zoom
+            plotURL={`api/results/${request}/quantification_cor.svg`}
+            className="border rounded p-3 mb-2"
+            maxHeight="800px"
+            descAbove={
+              <p>
+                The following asymmetric heatmap shows the pairwise correlation
+                among all the traits based on the quantification values. Both
+                Pearson and Spearman correlation coefficients were calculated as
+                upper and lower triangles in the heatmap, respectively.
+              </p>
+            }
+          />
+          <br />
+          <Zoom
+            plotURL={`api/results/${request}/quantification_dis.svg`}
+            className="border rounded p-3 mb-2"
+            maxHeight="800px"
+            descAbove={
+              <p>
+                The ridgeline density plot below indicates the distribution of
+                quantification for all of the traits and is sorted by the median
+                quantification value.
+              </p>
+            }
+          />
+          <br />
           <p>
-            The following asymmetric heatmap shows the pairwise correlation
-            among all the traits based on the quantification values. Both
-            Pearson and Spearman correlation coefficients were calculated as
-            upper and lower triangles in the heatmap, respectively.
+            A typical QTL barplot is shown below with statistical tests in the
+            subtitles. This plot is created by the ggbetweenstats function from
+            ggstatsplot package. Check the original ggstatsplot for the details
+            of the statistical tests. You could use the residuals as the
+            quantification value after regressing on the covariables if your
+            original QTL study has any covariables.
           </p>
-        }
-      />
-      <br />
-      <Zoom
-        plotURL={`api/results/${request}/quantification_dis.svg`}
-        className="border rounded p-3 mb-2"
-        maxHeight="800px"
-        descAbove={
           <p>
-            The ridgeline density plot below indicates the distribution of
-            quantification for all of the traits and is sorted by the median
-            quantification value.
+            Choose trait ID (e.g. ENSG00000183486.8) and Genotype ID (e.g.
+            21:42743496:C:T) for boxplot of the specific QTL association.
           </p>
-        }
-      />
-      <br />
-      <p>
-        A typical QTL barplot is shown below with statistical tests in the
-        subtitles. This plot is created by the ggbetweenstats function from
-        ggstatsplot package. Check the original ggstatsplot for the details of
-        the statistical tests. You could use the residuals as the quantification
-        value after regressing on the covariables if your original QTL study has
-        any covariables.
-      </p>
-      <p>
-        Choose trait ID (e.g. ENSG00000183486.8) and Genotype ID (e.g.
-        21:42743496:C:T) for boxplot of the specific QTL association.
-      </p>
-      <Form className="row justify-content-between">
-        <>
-          <div className="col-md-9">
-            <Form.Group className="row">
-              <Form.Group className="col-md-4">
-                <Form.Label id="quantification-trait" className="mb-0 mr-auto">Trait ID</Form.Label>
-                <Form.Control
-                  type="text"
-                  aria-labelledby="quantification-trait"
-                  id="qtls-quantification-trait"
-                  placeholder="None"
-                  disabled={!submitted}
-                  value={traitID}
-                  onChange={(e) => {
-                    dispatch(updateQTLsGWAS({ traitID: e.target.value }));
-                  }}
-                />
-              </Form.Group>
+          <Form className="row justify-content-between">
+            <>
+              <div className="col-md-9">
+                <Form.Group className="row">
+                  <Form.Group className="col-md-4">
+                    <Form.Label
+                      id="quantification-trait"
+                      className="mb-0 mr-auto"
+                    >
+                      Trait ID
+                    </Form.Label>
+                    <Form.Control
+                      type="text"
+                      aria-labelledby="quantification-trait"
+                      id="qtls-quantification-trait"
+                      placeholder="None"
+                      disabled={!submitted}
+                      value={traitID}
+                      onChange={(e) => {
+                        dispatch(updateQTLsGWAS({ traitID: e.target.value }));
+                      }}
+                    />
+                  </Form.Group>
 
-              <Form.Group className="col-md-4">
-                <Form.Label id="quantification-genotype" className="mb-0 mr-auto">Genotype ID</Form.Label>
-                <Form.Control
-                  type="text"
-                  aria-labelledby="quantification-genotype"
-                  id="qtls-quantification-genotype"
-                  placeholder="None"
-                  disabled={!submitted}
-                  value={genotypeID}
-                  onChange={(e) => {
-                    dispatch(updateQTLsGWAS({ genotypeID: e.target.value }));
-                  }}
-                />
-              </Form.Group>
+                  <Form.Group className="col-md-4">
+                    <Form.Label
+                      id="quantification-genotype"
+                      className="mb-0 mr-auto"
+                    >
+                      Genotype ID
+                    </Form.Label>
+                    <Form.Control
+                      type="text"
+                      aria-labelledby="quantification-genotype"
+                      id="qtls-quantification-genotype"
+                      placeholder="None"
+                      disabled={!submitted}
+                      value={genotypeID}
+                      onChange={(e) => {
+                        dispatch(
+                          updateQTLsGWAS({ genotypeID: e.target.value })
+                        );
+                      }}
+                    />
+                  </Form.Group>
 
-              <div className="col-md-4">
-                <Form.Label id="quantification-log" className="mb-0">
-                  log<sub>2</sub>
-                </Form.Label>
-                <ReactSelect
-                  isDisabled={!submitted}
-                  aria-labelledby="quantification-log"
-                  inputId="qtls-results-quantification-log2"
-                  value={log2}
-                  onChange={(option) => {
-                    dispatch(updateQTLsGWAS({ log2: option }));
-                  }}
-                  options={[
-                    { value: true, label: 'True' },
-                    { value: false, label: 'False' },
-                  ]}
-                  styles={{
-                    menuPortal: (base) => ({ ...base, zIndex: 9999 }),
-                  }}
-                  menuPortalTarget={document.body}
-                  filterOption={createFilter({ ignoreAccents: false })}
-                />
+                  <div className="col-md-4">
+                    <Form.Label id="quantification-log" className="mb-0">
+                      log<sub>2</sub>
+                    </Form.Label>
+                    <ReactSelect
+                      isDisabled={!submitted}
+                      aria-labelledby="quantification-log"
+                      inputId="qtls-results-quantification-log2"
+                      value={log2}
+                      onChange={(option) => {
+                        dispatch(updateQTLsGWAS({ log2: option }));
+                      }}
+                      options={[
+                        { value: true, label: 'True' },
+                        { value: false, label: 'False' },
+                      ]}
+                      styles={{
+                        menuPortal: (base) => ({ ...base, zIndex: 9999 }),
+                      }}
+                      menuPortalTarget={document.body}
+                      filterOption={createFilter({ ignoreAccents: false })}
+                    />
+                  </div>
+                </Form.Group>
               </div>
-            </Form.Group>
-          </div>
-          <div className="col-md-auto mt-4">
-            <Button
-              disabled={!submitted}
-              className="d-block"
-              variant="primary"
-              type="button"
-              onClick={() => handleRecalculate()}
-            >
-              Recalculate
-            </Button>
-          </div>
-        </>
-      </Form>
-      <Zoom
-        plotURL={`api/results/${request}/quantification_qtl.svg`}
-        className="border rounded p-3 mb-2"
-        maxHeight="800px"
-      />
+              <div className="col-md-auto mt-4">
+                <Button
+                  disabled={!submitted}
+                  className="d-block"
+                  variant="primary"
+                  type="button"
+                  onClick={() => handleRecalculate()}
+                >
+                  Recalculate
+                </Button>
+              </div>
+            </>
+          </Form>
+          <Zoom
+            plotURL={`api/results/${request}/quantification_qtl.svg`}
+            className="border rounded p-3 mb-2"
+            maxHeight="800px"
+          />
+        </div>
+      )}
     </div>
   );
 }
